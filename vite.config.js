9e8a92f34ff35a7a3f70aa9896207e6a097d8415
gaelivan740 <65014532+gaeliam100@users.noreply.github.com>
2023-06-08 01:45:57 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]})
+  plugins: [react()],
+  base:'https://gaeliam100.github.io/portefolioweb'
+})

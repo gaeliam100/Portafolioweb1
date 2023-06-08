7c81de6061127f878e3433ba32a3724b397b7e72
gaelivan740 <65014532+gaeliam100@users.noreply.github.com>
2023-06-08 03:05:50 -0600

    first commit

--- src/Components/portafolio/seconData.jsx
+++ src/Components/portafolio/seconData.jsx
@@ -4,20 +4,6 @@ import Work3 from '../../assets/Captura de pantalla 2023-05-30 a la(s) 13.50.32.
 import Work4 from '../../assets/mas claro.png'
 import Work5 from '../../assets/loginphp.png'
 import Work6 from '../../assets/todolist.png'
-import first from '../../assets/Proyecto1/First.png'
-import segunda from '../../assets/Proyecto1/segunda.png'
-import tercera from '../../assets/Proyecto1/tercera.png'
-import P2 from '../../assets/Proyecto2/bootstrapRegister.png'
-import P3 from '../../assets/Proyecto2/bootstrapRegister2.png'
-import P4 from '../../assets/Proyecto2/loginbootandnav.png'
-import E1 from '../../assets/Proyecto3/easy1.jpg'
-import E2 from '../../assets/Proyecto3/easy2.jpg'
-import E3 from '../../assets/Proyecto3/easy3.jpeg'
-import E4 from '../../assets/Proyecto3/easy4.jpeg'
-import E5 from '../../assets/Proyecto3/easy5.jpeg'
-import E6 from '../../assets/Proyecto3/easy6.jpeg'
-import E7 from '../../assets/Proyecto3/easy7.jpeg'
-
 const seconData = [
   {
     id: 1,
@@ -26,7 +12,7 @@ const seconData = [
     category: "Desarrollo web",
     information:"Aprendí sobre HTML, CSS y Javascript  creando una página web en tributo a uno de mis videojuegos favoritos.",
     evidencia:"",
-    imagenes:[first,segunda,tercera]
+    imagenes:[""]
   },
   {
     id: 2,
@@ -35,7 +21,7 @@ const seconData = [
     category: "MVC",
     information:"Aprendí como interactua el backend con el front end usando un patron de diseño en MVC,actualizaciones de estado y métodos para controlar las acciones,también implementé un chat en tiempo real con SignalR, aqui es donde adqurí conocimientos muy solidos de la programación ",
     evidencia:"",
-    imagenes:[E1,E2,E3,E4,E5,E6,E7]
+    imagenes:[""]
   },
   {
     id: 3,
@@ -44,7 +30,7 @@ const seconData = [
     category: "React",
     information:"Creando este portafolio aprendí como es que jsx combina lo mejor de javascript en la creación de componentes y como es muy facil interactuar con la información de manera estructurada",
     evidencia:"#",
-    imagenes:[first,segunda,tercera]
+    imagenes:[""]
   },
   ,
   {
@@ -54,7 +40,7 @@ const seconData = [
     category: "Desarrollo web",
     information:"de este aproyecto reafirme el conocimiento de mi curso de freecodecamp, creando una pagina para unos abogados en derecho familiiar,aprendí como es que funcionan los dominios y como hacer para colocar uno a tu web. Sitio: ",
     evidencia:"https://pensionalimenticia.org",
-    imagenes:[first,segunda,tercera]
+    imagenes:[""]
   },
   ,
   {
@@ -64,7 +50,7 @@ const seconData = [
     category: "laravel",
     information:"Me aventuré en el mundo de PHP y conocí laravel, cree un login con las herramientas que nos proporciona laravel para hacer las cosas más fáciles,utilicé composer y npm para instalar lo necesario, e implemente un registro de usuarios conectado a una base de datos mysql la cual estaba hecha con migraciones,aprendí a usa xammp y como configurar las rutas post get delete y update en esta tecnología",
     evidencia:"",
-    imagenes:[P2,P3,P4]
+    imagenes:[""]
   },
   ,
   {
@@ -74,7 +60,7 @@ const seconData = [
     category: "laravel",
     information:"Aqui dí mis primeros pasos en laravel, configueré el entorno de desarrollo y aprendí lo basico de php combinado con laravale, hice un CRUD de una lista de tareas implementando rutas para hacer las peticiones a la base de datos. Código:",
     evidencia:"https://github.com/gaeliam100/Proyectos-PHP",
-    imagenes:[first,segunda,tercera]
+    imagenes:[""]
   },
 ];
 export default seconData
